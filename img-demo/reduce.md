a85b972dd844e8a53e8c450c9f53d509ac4c8fdd
zhengpeng <847850277@qq.com>
2016-07-13 17:24:07 +0800

    创建图片的最简单的类

--- img-demo/reduce.md
+++ img-demo/reduce.md
@@ -325,11 +325,35 @@
 
 
 
-##用代码生成图片的小例子
+**最简单的生成图片的代码**
+---
 ```
 
+import java.awt.Graphics2D;
 
+import java.awt.image.BufferedImage;
 
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+/**
+ * 创建图像类
+ * @author Administrator
+ *
+ */
+
+public class CreateImage {
+
+	public static void main(String[] args) throws Exception{
+        
+        File file = new File("H:/image.jpg");   
+        BufferedImage bi = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);   
+        Graphics2D g2 = (Graphics2D)bi.getGraphics();   
+        ImageIO.write(bi, "jpg", file);
+           
+	}
+}
 ```
 
 
--- img-demo/src/main/java/com/zp/img_demo/CreateImage.java
+++ img-demo/src/main/java/com/zp/img_demo/CreateImage.java
@@ -2,15 +2,18 @@ package com.zp.img_demo;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-
+import java.io.File;
+import javax.imageio.ImageIO;
 /**
  * 创建图像类
  * @author Administrator
  *
  */
 public class CreateImage {
-	public static void main(String[] args) {
-		BufferedImage bi = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
-		Graphics2D graphics2D =  (Graphics2D)bi.getGraphics();
+	public static void main(String[] args) throws Exception{
+        File file = new File("H:/image.jpg");   
+        BufferedImage bi = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);   
+        Graphics2D g2 = (Graphics2D)bi.getGraphics();   
+        ImageIO.write(bi, "jpg", file);   
 	}
 }
